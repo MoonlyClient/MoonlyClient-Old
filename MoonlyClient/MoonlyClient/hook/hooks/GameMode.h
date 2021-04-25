0963782354de427b65838d79fffb22cc2063fd8c
Seyz <seylio@outlook.com>
2021-04-25 15:48:55 +0200

    Fix GameMode hook for 1.16.221

--- MoonlyClient/MoonlyClient/hook/hooks/GameMode.h
+++ MoonlyClient/MoonlyClient/hook/hooks/GameMode.h
@@ -15,6 +15,8 @@ GmTick _GmTick;
 GmTick _SmTick;
 
 void GmTick_callback(GameMode* GM) {
+	Utils::DebugLogOutput("GameMode::tick called lmao");
+
 	LocalPlayer* Player = gData.getClientInstance()->LocalPlayer();
 
 	if (GM != nullptr && GM->Player != nullptr && Player != nullptr) {
@@ -38,6 +40,8 @@ void GmTick_callback(GameMode* GM) {
 }
 
 void SmTick_callback(GameMode* GM) {
+	Utils::DebugLogOutput("SurvivalGameMode::tick called lmao");
+
 	LocalPlayer* Player = gData.getClientInstance()->LocalPlayer();
 
 	if (GM != nullptr && GM->Player != nullptr && Player != nullptr) {
@@ -61,7 +65,7 @@ void SmTick_callback(GameMode* GM) {
 }
 
 void GameModeHook::install() {
-	//this->hookSig("GameMode::tick", "8B 41 20 89 41 1C C3", &GmTick_callback, reinterpret_cast<LPVOID*>(&_GmTick));
+	this->hookSig("GameMode::tick", "40 53 48 83 EC 20 48 8B D9 48 8B 89 ? ? ? ? 48 8B 01 FF 50 ? 8B 43 ?", &GmTick_callback, reinterpret_cast<LPVOID*>(&_GmTick));
 
 	//this->hookSig("SurvivalMode::tick", "48 89 5C 24 ?? 48 89 74 24 ?? 55 57 41 56 48 8D 6C 24 B9 48 81 ?? ?? ?? ?? ?? 48 8B 05 0F F3 8A 01", &SmTick_callback, reinterpret_cast<LPVOID*>(&_SmTick));
 }
